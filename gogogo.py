df94f271df92e510918fc921c75ba0fe5dc158c3
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-03-10 21:15:26 -0500

    initial commit

---
+++ gogogo.py
@@ -0,0 +1,45 @@
+# set up maptlotlib to use agg
+# taken from sphinx-gallery
+# https://github.com/sphinx-gallery/sphinx-gallery/blob/4a026660b2cbb00c3ce8a7e28e957d8ef6320099/sphinx_gallery/scrapers.py#L31
+import matplotlib
+from warnings import filterwarnings
+
+matplotlib.use("agg")
+
+filterwarnings(
+    "ignore",
+    category=UserWarning,
+    message="Matplotlib is currently using agg, which is a"
+    " non-GUI backend, so cannot show the figure.",
+)
+
+import matplotlib.pyplot as plt
+import glob
+import time
+
+files = glob.glob("examples/**/*.py")
+times = []
+for f in files:
+    start = time.time()
+    try:
+        exec(open(f).read())
+    except:
+        print(f)
+        continue
+    end = time.time()
+    times.append(end - start)
+    plt.close("all")
+from datetime import datetime
+
+now = "data/" + datetime.now().strftime("%Y-%m-%d_%I-%M") + ".csv"
+from matplotlib import __version__ as mpl_version
+import sys
+import platform
+
+with open(now, "w") as f:
+    f.write("# matplotlib version:" + mpl_version + "\n")
+    for l in sys.version.splitlines():
+        f.write("# " + l + "\n")
+    f.write("# " + platform.platform() + "\n")
+    for fname, time in zip(files, times):
+        f.write(f"{fname}, {time}\n")
