dbeb926220fc4cf34a7de0c41e4fbcd83328ee57
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-03-18 15:47:52 -0400

    smoother plotting

--- plottttt.py
+++ plottttt.py
@@ -6,26 +6,33 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 files = glob.glob("data/*.csv")
-times = np.zeros([len(files), 234])
-print(times.shape)
+times = np.zeros([len(files), 445])
 for i, f in enumerate(files):
     arr = np.genfromtxt(f, dtype=object)
     times[i] = arr[:, 1].astype(float)
 
 
-style = {"linestyle": "--", "color": "k", "alpha": 0.75}
+style = {"linestyle": "--", "color": "k", "alpha": 0.25}
 fig, ax = plt.subplots()
 ax.plot(times, **style)
-# ax.plot(times[:,0], **style)
-# ax.plot(times[:,10], **style)
-# plt.show()
+ax.set_xlabel('Run number')
+ax.set_ylabel('Time (s)')
+ax.set_title('Absolute Times')
 
 # normalized by means
 means = times.mean(axis=0, keepdims=True)
-print(means)
-print(means.shape)
 fig, ax = plt.subplots()
 ax.plot(times / means, **style)
-# ax.plot(times[:,0], **style)
-# ax.plot(times[:,10], **style)
-plt.show()
+ax.set_title('Time normalized by mean time')
+ax.set_xlabel('Run number')
+ax.set_ylabel('Relative Time')
+
+
+# # most and least consistent
+# fig, axs = plt.subplots(2, 1)
+# CVs = times.std(axis=0)  / times.mean(axis=0)
+# max_idx = np.argmax(CVs)
+# min_idx = np.argmin(CVs)
+# axs[0].plot(times[:, max_idx])
+# axs[1].plot(times[:, min_idx])
+# plt.show()
